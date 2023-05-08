fbac29c5e35f55014a478abc72ff3c7d66a87584
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-05-08 21:23:33 +0900

    Update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,14 @@
-import { parse } from "./index";
+import { parse, get_name } from "./index";
 
 test("main", () => {
   const src = "int main(void) { return a + 1.2; }";
-  expect(parse(src)).toStrictEqual({
-    result: true,
-  });
+  const ast = parse(src).get_top();
+  const name = get_name(ast.list[0]);
+  expect(name).toStrictEqual("main");
 });
 test("decl", () => {
   const src = "int x = 3;";
-  expect(parse(src)).toStrictEqual({ result: true });
+  const ast = parse(src).get_top();
+  const name = get_name(ast.list[0]);
+  expect(name).toStrictEqual("x");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,81 @@
 import Scanner = require("../generated/scanner");
 
 const parser = new Scanner.CParser();
-type ObjectType<T> = { [key: string]: T };
-type ParseIndex = number | number[] | null;
 type ParseElement<K extends string, A extends number> = {
+  type: string;
   kind: K;
   age: A;
   id: number;
-  children: { type?: string; value?: string } & { [key: string]: ParseIndex };
+  children: number[];
 };
 type AstElement = ParseElement<"ast", 0>;
+type RawAstType = {
+  type: string;
+  children: RawAstType[];
+  [key: string]: any;
+};
 type ParseResult = {
   root: AstElement[];
   top: number;
-  construct: (id: number) => object;
-  get: () => string;
+  construct: (id: number) => RawAstType;
+  get_top: () => RawAstType;
 };
 
-const object_values_map = <T, U>(
-  obj: ObjectType<T>,
-  f: (value: T) => U
-): ObjectType<U> =>
-  Object.entries(obj).reduce(
-    (acc, [key, value]) => ({ ...acc, [key]: f(value) }),
-    {}
-  );
 export function parse(input: string): ParseResult {
+  const ast: RawAstType = parser.parse(input);
+  const root: AstElement[] = [];
+  const f = (node: RawAstType): number => {
+    const children = node.children.map((child) => f(child));
+    const id = root.length;
+    root.push({ ...node, kind: "ast", age: 0, id, children });
+    return id;
+  };
+  const top = f(ast);
   const result: ParseResult = {
-    ...parser.parse(input),
+    root,
+    top,
     construct(id: number) {
-      return object_values_map(this.root[id].children, (value) => {
-        if (typeof value === "number") {
-          return this.construct(value);
-        } else if (Array.isArray(value)) {
-          return value.map((id) => this.construct(id));
-        } else {
-          return value;
-        }
-      });
+      const ast = this.root[id];
+      return { ...ast, children: ast.children.map((id) => this.construct(id)) };
     },
-    get() {
-      return JSON.stringify(this.construct(this.top));
+    get_top() {
+      return this.construct(this.top);
     },
   };
   return result;
 }
-function run(input: string) {
-  const result = parse(input);
-  console.log(input.trim(), "=", result.get());
-}
+export const get_name = (decl: any): string | null => {
+  if (decl.type === "declaration") {
+    return get_name(decl.init_declarator_list);
+  } else if (decl.type === "init_declarator_list") {
+    const list = decl.list.map(get_name);
+    return list.length === 1 ? list[0] : list;
+  } else if (decl.type === "init_declarator") {
+    return get_name(decl.declarator);
+  } else if (decl.type === "declarator") {
+    return get_name(decl.direct_declarator);
+  } else if (decl.type === "identifier_direct_declarator") {
+    return decl.identifier.value;
+  } else if (decl.type === "paren_direct_declarator") {
+    return get_name(decl.declarator);
+  } else if (
+    decl.type === "bracket_direct_declarator" ||
+    decl.type === "parameter_direct_declarator" ||
+    decl.type === "old_direct_declarator"
+  ) {
+    return get_name(decl.direct_declarator);
+  } else if (decl.type === "external_declaration") {
+    return get_name(decl.declaration);
+  } else if (decl.type === "function_definition") {
+    return get_name(decl.declarator);
+  } else {
+    return null;
+  }
+};
 
+function run(input: string) {
+  const result = parse(input).get_top();
+  console.log(input.trim(), "=", result);
+}
 run("int main(void) { return 0; }");
 run("int x; float y;");
-
-function yyscan_is_typedef(str: string): boolean {
-  return false;
-}
