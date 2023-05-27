8a442030d04ad204d52509e42fdfde0d8c00908a
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-05-27 13:33:50 +0900

    Add module

---
+++ src/module.ts
@@ -0,0 +1,120 @@
+import assert from "assert";
+import { combineObjects, makeSingleton, smartMap } from "./util";
+
+type Id = number;
+type IdValue = Id | Id[] | null;
+
+type NodeValue = Record<string, IdValue>;
+export class ModuleNode {
+  type: string;
+  token: string | null;
+  value: NodeValue;
+  constructor(args: { type: string; token: string | null; value: NodeValue }) {
+    this.type = args.type;
+    this.token = args.token;
+    this.value = args.value;
+  }
+
+  get(key: string): IdValue {
+    return key in this.value ? this.value[key] : null;
+  }
+
+  clone(value?: NodeValue): ModuleNode {
+    return new ModuleNode({ ...this, value: value ?? {} });
+  }
+}
+
+type TransformMap = (
+  node: ModuleNode,
+  get: (id: Id) => ModuleNode
+) => ModuleNode | undefined;
+type VisitorMap = (node: ModuleNode) => string[];
+export class Module {
+  root: ModuleNode[] = [];
+  age: number = 0;
+
+  constructor(list: ModuleNode[], top: Id) {
+    const t = new Transformer(list, () => undefined);
+    t.transform(top);
+    this.root = t.next;
+  }
+
+  transform(map: TransformMap): void {
+    const t = new Transformer(this.root, map);
+    t.transform(0);
+    this.root = t.next;
+    this.age += 1;
+  }
+
+  visit(map: VisitorMap): void {
+    new Visitor(this.root, map).visit(0);
+  }
+}
+
+class Transformer {
+  readonly prev: ModuleNode[];
+  next: ModuleNode[] = [];
+  table: Record<Id, Id> = {};
+  readonly map: TransformMap;
+
+  constructor(prev: ModuleNode[], map: TransformMap) {
+    this.prev = prev;
+    this.map = map;
+  }
+
+  transform(id: Id): Id {
+    assert(!this.done(id));
+    const [nextId, prevNode] = this.prepareNext(id);
+    const nextNode = this.getNext(prevNode);
+    this.next[nextId] = nextNode ?? this.getDefaultNext(prevNode);
+    return nextId;
+  }
+
+  private prepareNext(prevId: Id): [Id, ModuleNode] {
+    assert(prevId < this.prev.length);
+    const prevNode = this.prev[prevId];
+    const nextId = this.next.length;
+    this.next.push(prevNode.clone());
+    this.table[prevId] = nextId;
+    return [nextId, prevNode];
+  }
+
+  private getNext(prevNode: ModuleNode): ModuleNode | undefined {
+    return this.map(prevNode, (id) => this.next[this.get(id)]);
+  }
+
+  private getDefaultNext(prevNode: ModuleNode): ModuleNode {
+    const get = (id: Id): Id => this.get(id);
+    const nextEntries = Object.entries(prevNode.value).map(([k, v]) =>
+      makeSingleton(k, smartMap(v, get))
+    );
+    return prevNode.clone(combineObjects<string, IdValue>(nextEntries));
+  }
+
+  private done(id: Id): boolean {
+    return id in this.table;
+  }
+
+  private get(id: Id): Id {
+    return this.done(id) ? this.table[id] : this.transform(id);
+  }
+}
+
+class Visitor {
+  readonly root: ModuleNode[];
+  readonly map: VisitorMap;
+
+  constructor(root: ModuleNode[], map: VisitorMap) {
+    this.root = root;
+    this.map = map;
+  }
+
+  visit(id: Id): void {
+    assert(id < this.root.length);
+    const node = this.root[id];
+    const visit = (id: Id): void => {
+      this.visit(id);
+    };
+    this.map(node).forEach((key) => smartMap(node.get(key), visit));
+  }
+}
--- src/util.ts
+++ src/util.ts
@@ -10,13 +10,14 @@ export const isObject = (x: unknown): x is object =>
 export const isNumberArray = (x: unknown): x is number[] =>
   isArray(x) && x.every(isNumber);
 
+type PRecord<K extends PropertyKey, T> = Partial<Record<K, T>>;
 type MapType<T, U> = (x: T) => U | undefined;
 export function smartMap<T, U>(x: T, f: MapType<T, U>): U;
 export function smartMap<T, U>(x: T[], f: MapType<T, U>): U[];
-export function smartMap<T, U>(
-  x: Partial<Record<string, T>>,
+export function smartMap<K extends string, T, U>(
+  x: PRecord<K, T>,
   f: MapType<T, U>
-): Record<string, U>;
+): Record<K, U>;
 export function smartMap<T, U>(x: null, f: MapType<T, U>): null;
 export function smartMap<T, U>(x: undefined, f: MapType<T, U>): undefined;
 export function smartMap<T, U>(
@@ -27,20 +28,36 @@ export function smartMap<T, U>(
   x: T | T[] | null,
   f: MapType<T, U>
 ): U | U[] | null;
-export function smartMap<T, U>(
-  x: T | T[] | Partial<Record<string, T>> | null | undefined,
+export function smartMap<K extends string, T, U>(
+  x: T | T[] | PRecord<K, T> | null | undefined,
   f: MapType<T, U>
-): U | U[] | Record<string, U> | null | undefined {
+): U | U[] | Record<K, U> | null | undefined {
   if (isArray(x)) {
     return x.map(f).filter(isDefined);
   } else if (isObject(x)) {
-    return Object.keys(x)
-      .map((k) => smartMap(smartMap(x[k], f), (v: U) => ({ [k]: v })) ?? {})
-      .reduce((prev, next) => ({ ...prev, ...next }), {});
+    const g = (k: string): Record<K, U> =>
+      smartMap(smartMap(x[k as K], f), (v: U) => makeSingleton(k, v)) ??
+      initObject();
+    return combineObjects<K, U>(Object.keys(x).map(g));
   } else {
     return isNull(x) || isUndefined(x) ? x : f(x);
   }
 }
+export const combineObjects = <K extends PropertyKey, T>(
+  x: Array<PRecord<K, T>>
+): Record<K, T> =>
+  x.reduce<Record<K, T>>((prev, next) => ({ ...prev, ...next }), initObject());
+const initObject = <T extends object>(): T => {
+  const obj = {};
+  return obj as T;
+};
+export const makeSingleton = <K extends PropertyKey, V>(
+  k: K,
+  v: V
+): Record<K, V> => {
+  const obj = { [k]: v };
+  return obj as Record<K, V>;
+};
 
 export const hexlify = (str: string): string => {
   return str
