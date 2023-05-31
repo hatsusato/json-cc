8686c13496fd035afe882d8ab7cb323f3d1cfd6c
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-06-01 02:24:56 +0900

    Read source file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Module } from "./module";
+import { readFileSync } from "fs";
+import { getName, parseAst } from "./ast";
+import { type Module } from "./module";
 
 export const compile = (module: Module): string => {
+  const name = getName(module, 0);
   return [
     ...module.emitHeader(),
-    "define dso_local i32 @main() {",
+    `define dso_local i32 @${name}() {`,
     "  %1 = alloca i32, align 4",
     "  store i32 0, i32* %1, align 4",
     "  ret i32 0",
@@ -16,6 +19,7 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 process.argv.slice(2).forEach((source) => {
-  const module = new Module([], 0, source);
+  const input = readFileSync(source, "utf8");
+  const module = parseAst(input, source);
   console.log(compile(module));
 });
