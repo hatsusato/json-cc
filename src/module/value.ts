b3d9c4d89947682dedb8e1be6c4be59ec7822ce3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-07 16:42:17 +0900

    Add clone and newValue

--- src/module/value.ts
+++ src/module/value.ts
@@ -3,7 +3,7 @@ import { expandValue } from "./visit";
 
 export class ValueRef {
   readonly id: Id;
-  private module: Module;
+  private readonly module: Module;
   constructor(id: Id, module: Module) {
     this.id = id;
     this.module = module;
@@ -15,17 +15,25 @@ export class ValueRef {
 
 export class Value {
   private ref: ValueRef;
+  private module: Module;
   type: string;
   symbol?: string;
   list?: Value[];
   children: Record<string, Value> = {};
   constructor(module: Module, id: Id, type: string) {
     this.ref = new ValueRef(id, module);
+    this.module = module;
     this.type = type;
   }
   show(stringify: boolean = true): string | object {
     const value = expandValue(this);
     return stringify ? JSON.stringify(value, undefined, 2) : value;
+  }
+  clone(): Value {
+    return this.module.cloneValue(this);
+  }
+  newValue(type: string): Value {
+    return this.module.createValue(type);
   }
   get id(): Id {
     return this.ref.id;
