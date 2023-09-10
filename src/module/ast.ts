34f1739e57befe9223a228a820bb123321b4d2ed
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-10 17:03:18 +0900

    Simplify AstVisitor

--- src/module/ast.ts
+++ src/module/ast.ts
@@ -1,38 +1,35 @@
 import assert from "assert";
-import { asString, isArray, isObject, objMap } from "../util";
+import { isArray, isObject, isString, objMap } from "../util";
 import { newList, newSymbol } from "./node";
 import { getNull, getPool } from "./pool";
 import type { Node } from "./types";
 
-class AstVisitor {
-  constructor() {
-    const pool = getPool();
+const isValidAst = (
+  ast: unknown
+): ast is null | unknown[] | { symbol: string } | { type: string } =>
+  ast === null ||
+  isArray(ast) ||
+  (isObject(ast) && "symbol" in ast && isString(ast.symbol)) ||
+  (isObject(ast) && "type" in ast && isString(ast.type));
+const visit = (key: string, ast: unknown): Node => {
+  assert(isValidAst(ast));
+  if (ast === null) {
+    return getNull();
+  } else if ("type" in ast) {
+    key = ast.type;
   }
-  visit(key: string, ast: unknown): Node {
-    if (ast === null) {
-      return getNull();
-    } else {
-      assert(
-        isArray(ast) || (isObject(ast) && ("symbol" in ast || "type" in ast))
-      );
-      if ("type" in ast) {
-        key = asString(ast.type);
-      }
-    }
-    const node = getPool().createNode(key);
-    if (isArray(ast)) {
-      node.children.list = newList(ast.map((x) => this.visit(key, x)));
-    } else if ("symbol" in ast) {
-      node.children.symbol = newSymbol(asString(ast.symbol));
-    } else if ("type" in ast) {
-      const { type: _, ...children } = ast;
-      node.children = objMap(children, ([k, v]) => this.visit(k, v));
-    }
-    return node;
+  const node = getPool().createNode(key);
+  if (isArray(ast)) {
+    node.children.list = newList(ast.map((x) => visit(key, x)));
+  } else if ("symbol" in ast) {
+    node.children.symbol = newSymbol(ast.symbol);
+  } else if ("type" in ast) {
+    const { type: _, ...children } = ast;
+    node.children = objMap(children, ([k, v]) => visit(k, v));
   }
-}
+  return node;
+};
 export const convert = (ast: unknown): Node => {
-  const visitor = new AstVisitor();
-  const top = visitor.visit("top", ast);
+  const top = visit("top", ast);
   return top.children.translation_unit;
 };
