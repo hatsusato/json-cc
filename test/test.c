27242c9d33b4ec92aa688801d118afc79ca435b7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-12 01:09:36 +0900

    Initial test case

---
+++ test/test.c
@@ -0,0 +1,20 @@
+int f0(void) {
+  return 0;
+}
+
+int f1(void) {
+  int x;
+  return 0;
+}
+
+int f2(void) {
+  int x;
+  x = 1;
+  return 0;
+}
+
+int f3(void) {
+  int x;
+  x = 1;
+  return x;
+}
